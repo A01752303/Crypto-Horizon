0c5467a8efaee4a9b76d8ef6ac5ca217fc0e3646
A01752303 <a01752303@tec.mx>
2025-04-23 02:06:04 -0600

    CryptoFacts

---
+++ Assets/Script/Enter level/fact02.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+namespace fact02
+{
+    public class fact : MonoBehaviour
+    {
+        [SerializeField] private GameObject r;
+        private bool playerInTrigger = false;
+        [SerializeField] private GameObject banner1;
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                r.SetActive(true);
+                playerInTrigger = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                r.SetActive(false);
+                playerInTrigger = false;
+            }
+        }
+
+        private void Update()
+        {
+            if (playerInTrigger && Input.GetKeyDown(KeyCode.R))
+            {
+                banner1.SetActive(true);
+            }
+        }
+
+        public void exitFact()
+        {
+            banner1.SetActive(false);
+        }
+    }
+}
